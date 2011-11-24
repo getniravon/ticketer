94b2372a2121bb336d3cfee8add74882103ed62d
Nirav Soni <nsoni3@hawk.iit.edu>
2011-11-24 08:47:07 +0530

    generated the rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketer::Application.config.secret_token = '954cc75e33f8f537616d350a32d513b9de13751e817819e0a32a54a70b2f980193d8cacb3eba4f0a0ab2d556c8b067dfe8205b4ff4dfaf1e0fdfc6e65c7d8a85'
